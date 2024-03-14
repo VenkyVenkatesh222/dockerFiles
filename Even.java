513c8448610ccc31ee403d6cb0b6a9b9d0eb55dd
venkatesh kengam <venkatesh@DESKTOP-MGEPR81>
2024-03-14 11:26:54 +0530

    adding java even code file

---
+++ Even.java
@@ -0,0 +1,20 @@
+
+public class Even{
+	public static void main(String [] args)
+	{
+         // Scanner sc = new Scanner(System.in);
+	  int y = 4;
+	  if(y==1)
+	  {
+		  System.out.println("not an even number"+y);
+	  }
+	  else if(y<1)
+	  {
+		 System.out.println("invalid input");
+	  }
+	  else if(y%2==0)
+	  {
+		 System.out.println("even number"+y);
+	  }
+	}
+}	
